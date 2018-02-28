96972905c36c39c502235ed06b61f7079f654f9c
wsun <wangjia.sun@gmail.com>
2018-02-28 11:03:42 +0000

    mis: fix code smells

--- analyse.py
+++ analyse.py
@@ -4,13 +4,13 @@ from lib.strategy_finding.data_labeler.dnb_win_only import DnbWinOnly
 from lib.strategy_finding.feature_builder.wdl_probabilities import WDLProbabilitiesFeatureBuilder
 from lib.strategy_finding.algorithm.random_forest import RandomForestAlgorithm
 
-path_to_dir = 'data/test/'
+path_to_dir = 'data/'
 analyser = Analyser()
 games_data = analyser.load_data(path_to_dir)
 print(analyser.execute(
-    TrendingOnlySelector(),
-    DnbWinOnly(),
-    WDLProbabilitiesFeatureBuilder(),
-    RandomForestAlgorithm(),
-    games_data
+    sample_selector = TrendingOnlySelector(),
+    data_labeler = DnbWinOnly(),
+    feature_builder = WDLProbabilitiesFeatureBuilder(),
+    algorithm = RandomForestAlgorithm(),
+    data = games_data
 ))
