f6a11dfe64a8647ebbfb4ee556ec13e143ebcbfc
Wang Yao <wyao@lud-sgpwy01.US.DRWHoldings.com>
2018-08-10 21:41:59 +0800

    fix test

--- misc/test_rf_6_leagues.py
+++ misc/test_rf_6_leagues.py
@@ -3,16 +3,19 @@ import json
 game_qualifier = RF6Leagus()
 
 # Put your game data here to test
-data = json.loads(open('./data/football_all_odds_data/English Premier League-2016-2017.json').read())
 i = 0
 j = 0
-for game in data:
-	result = game_qualifier.is_game_qualified(data)
-	print(result)
-	if result is not False:
-		i += 1
-	else:
-		j += 1
+
+file_name = "/home/wyao/workspace/ot-aws/data/football_all_odds_data/English Premier League-2016-2017.json"
+with open(file_name) as json_file:
+    matches = json.load(json_file)
+    for data in matches:
+        result = game_qualifier.is_game_qualified(data)
+        print(result)
+        if result is not False:
+            i += 1
+        else:
+            j += 1
 
 print (str(i) + " games qualified")
 print (str(j) + " games disqualified")
