d790be8b0bdf1847f99ee9b40a03d2fcb9be5c7a
wsun <wangjia.sun@gmail.com>
2018-08-12 20:11:12 +0100

    dev: add email sending function as short term solution.

--- game_bet_placer.py
+++ game_bet_placer.py
@@ -6,6 +6,8 @@ import datetime
 kafka_topic_input = 'event-game-qualified'
 kafka_topic_output = 'event-bet-placed'
 
+recipient = "wangjia.sun@gmail.com, panda0079@gmail.com"
+
 producer = KafkaProducer(value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 consumer = KafkaConsumer(
 				kafka_topic_input,
@@ -25,5 +27,5 @@ while True:
 		print(game_data)
 		# Place bet & send results to Kafka
 		subject = str(game_data['home_team_name']) + ' vs ' + str(game_data['away_team_name']) + ' at ' + str(datetime.datetime.now())
-		mailer.send_email('wangjia.sun@gmail.com', subject, str(game_data))
+		mailer.send_email("wangjia.sun@gmail.com, panda0079@gmail.com", subject, str(game_data))
 	
