e622a7d427d1bb213b98dddd0be67763eaef44a7
wsun <wangjia.sun@gmail.com>
2018-12-03 22:59:15 +0000

    fix: return requests.get() results

--- src/crawler/browser_requests.py
+++ src/crawler/browser_requests.py
@@ -10,4 +10,4 @@ class BrowserRequests:
         print('Handling URL - ' +  url)
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-        requests.get(url, headers=headers, timeout=5)
+        return  requests.get(url, headers=headers, timeout=5)
