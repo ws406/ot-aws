1854e97050d2ea87f6934571285738aabd3ba35f
wangjias <wangjia.sun@thetrainline.com>
2019-07-17 07:45:39 +0100

    misc: move unneeded files to 'misc' dev: set up structure for 'true odds' betting strategy - added 'qualifier' file

--- fb_games_detector.py
+++ fb_games_detector.py
@@ -20,11 +20,50 @@ class Main:
     minutes = 15
     league_ids = [
         34,  # IT1
+        # 40,  # IT2
+        #
         36,  # EPL
         37,  # ENC
+        39,  # EFL1
+        35,  # EFL2
+        #
         31,  # ES1
+        33,  # ES2
+        #
         8,  # GE1
+        9,  # GE2
+        #
+        11,  # FR1
+        12,  # FR2
+        #
+        # 13, # FIN1
+        # 700, # THAI1
+        #
         16,  # HO1
+        17,  # HO2
+        #
+        25,  # JAP1
+        284,  # JAP2
+        #
+        4,  # BRA1
+        358,  # BRA2
+        #
+        23,  # POTG1
+        29,  # SCOT1
+        30,  # TUR1
+        5,  # BEL1
+        26,  # SWE1
+        22,  # NOR1
+        # 27,  # SWl1
+        10,  # RUS1
+        # 2,  # ARG1
+        21,  # USA1
+        # 415,  # CHILE1
+        # 140,  # MEX1
+
+        60,  # CHN1
+        15,  # KOR1
+        273,  # AUS
     ]
 
     gameDetector = None
---
+++ src/ops/game_qualifier/fb_true_odds.py
@@ -0,0 +1,13 @@
+from src.ops.game_qualifier.interface import GameQualifierInterface
+
+
+# The qualifier will make every game qualify.
+# *** The purpose is to provide true odds for all possible results ***
+class TrueOdds(GameQualifierInterface):
+
+    def __init__(self):
+        pass
+
+    def is_game_qualified(self, data):
+
+        pass
