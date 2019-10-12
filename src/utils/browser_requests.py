d0044b860e4b55d4d956ab5e48badf38cabc738e
wangjias <wangjia.sun@thetrainline.com>
2019-10-12 15:06:35 +0100

    fix: update request header

--- src/utils/browser_requests.py
+++ src/utils/browser_requests.py
@@ -10,8 +10,8 @@ class BrowserRequests:
     def get(url, logger: OtLogger):
         logger.log('Handling URL - ' +  url)
         headers = {
-            'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-        response = requests.get(url, headers=headers, timeout=20)
+            "content-type":"text"}
+        response = requests.get(url, headers=headers, timeout=10)
         if response.status_code != 200:
             raise requests.HTTPError
 
