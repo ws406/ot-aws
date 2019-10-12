0743bf6345aceec38e5910211463872d814ca968
wangjias <wangjia.sun@thetrainline.com>
2019-10-12 15:21:31 +0100

    fix: update request header

--- src/utils/browser_requests.py
+++ src/utils/browser_requests.py
@@ -10,8 +10,7 @@ class BrowserRequests:
     def get(url, logger: OtLogger):
         logger.log('Handling URL - ' +  url)
         params = {
-            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.90 Safari/537.36"
         }
         response = requests.get(url, params=params, timeout=10)
         if response.status_code != 200:
