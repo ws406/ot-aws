604b0187754964e58b98737b581dcc5b11e7f78e
wangjias <wangjia.sun@thetrainline.com>
2019-10-20 12:31:15 +0100

    misc: extend httprequest timeout to 20 seconds

--- src/utils/browser_requests.py
+++ src/utils/browser_requests.py
@@ -11,7 +11,7 @@ class BrowserRequests:
         logger.log('Handling URL - ' +  url)
         headers = {
             'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
-        response = requests.get(url, headers=headers, timeout=10)
+        response = requests.get(url, headers=headers, timeout=20)
         if response.status_code != 200:
             raise requests.HTTPError
 
